6d59a6eddcffc00a0f8f02d22822b99eb8ef5160
Shivmohan <nshivmohan08@gmail.com>
2026-09-15 09:22:17 +0530

    Removed note url from permit all

--- auth-service/src/main/java/com/micro/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/micro/auth/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**","/note/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(SWAGGER_WHITELIST).permitAll()
 //                        .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
